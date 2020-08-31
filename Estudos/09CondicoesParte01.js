1c49fcc8f164b43cb50dd0b352067b286e9dd066
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2020-08-31 16:36:40 -0300

    class 11

---
+++ Estudos/09CondicoesParte01.js
@@ -0,0 +1,16 @@
+// exemplo condicao simples:
+var velocidade = 58.6
+console.log(`a velocidade do seu carro eh: ${velocidade}Km/h`)
+if (velocidade > 60) {
+    console.log(`Voce ultrapassou a velocidade maxima da via, recebeu uma multa de R$300,00`)
+}
+console.log('Dirija sempre utilizando o sinto de seguranca!')
+
+// exemplo condicao composta
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Brasileiro!')
+} else {
+    console.log('Estrangeiro!')
+}
