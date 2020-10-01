63880a1199c9c0ff3b9044da37e104a6c1af81ee
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2020-10-01 19:22:49 -0300

    finish aula 14

--- Estudos/12RepeticoesParte02.js
+++ Estudos/12RepeticoesParte02.js
@@ -1,3 +1,7 @@
-for (var contador =1; contador < 10; contador++ ) {
+//       variavel      teste          incremento
+for (var contador = 1; contador < 10; contador++ ) {
     console.log(`fazendo pela ${contador} vez`)
 }
+
+// utilize a ferramenta de debug para ter uma experiencia completa da aula
+
