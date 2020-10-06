aba6947add22f9e31df1fa8409ef9e21f0012987
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2020-10-05 21:23:19 -0300

    refresh ex003

--- Exercicios/Ex003/script.js
+++ Exercicios/Ex003/script.js
@@ -1,15 +1,27 @@
 function contar() {
     
-var inicio = document.getElementById('inicio')
-var fim = document.getElementById('fim')
-var passo = document.getElementById('passo')
-var resultado = document.getElementById('resultado')
+    var inicio = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
+    var resultado = document.getElementById('resultado')
 
-if (inicio.value == 0 || fim.value == 0 || passo.value == 0) {
-    alert('Preencha todos os dados')
-}
-for (var contador = inicio.value; contador <= fim.value; contador + passo.value) {
-    resultado.innerHTML = `${contador} ->`
-}
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('Preencha todos os dados')
+    } else {
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i < f){
+        for (var contador = i; contador <= f; contador += p) {
+            resultado.innerHTML += ` ${contador} ->`
+        }
+        } else {
+            for (var contador1 = i; contador1 >= f; contador1 -= p) {
+                resultado.innerHTML += ` ${contador1} -> `
+            }
+        }
+        
+    }
+    
 
 }
